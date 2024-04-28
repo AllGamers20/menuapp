bd6cdf2c914952917f46054509bd9b8438eaf7df
AllGamers20 <husseinmoubarackmonster@gmail.com>
2024-04-28 18:14:32 +0300

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 'use client';
 import NavBar from "@/components/atom/NavBar";
+import Card from "@/components/section/Card";
 import HeadLine from "@/components/section/HeadLine";
 export default function page() {
   return <>
     <NavBar/>
+    <HeadLine title="Deserts"/>
+    <Card/>
+    
   </>;
 }
