ec49a19f38de051e9bc3bb0b920c90174f0adf45
AllGamers20 <husseinmoubarackmonster@gmail.com>
2024-05-01 21:02:05 +0300

    update

--- components/section/ProductsList.tsx
+++ components/section/ProductsList.tsx
@@ -4,7 +4,7 @@ export default function ProductList({ category }: any) {
   return (
     <div className="flex flex-wrap gap-2 p-2">
       {category.products.map((product: any) => (
-        <Card product={product} />
+        <Card product={product} key={category.key} />
       ))}
     </div>
   );
